6cd31adc821c92770cd0fc5df01a7d59495497b7
Kim Knudsen <kimknuds@dmf.int>
2017-06-22 16:07:58 +0200

    Starte med tabell for fremvisning av data

--- App.jsx
+++ App.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import Dyr from './Components/Dyr.js';
+import TabellKolonner from './Components/TabellKolonner.js'
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			bilde1: "http://www.zoologen.no/images/Katttre.jpg",
+			bilde2: "https://www.zoopermarked.no/resources/category/1_h/und/1%20hund.jpg?width=400&height=230&format=jpg",
+			dok_id: 1111,
+			dok_navn: "NAVN1",
+			datoer: ["01.01.2001","02.02.2002","03.03.2003"],
+			kommuner: ["Trondheimen", "TrondheimTo"],
+			stedsnavn: ["Lade","Sentrum","Lade Gård"],
+			mineraler: ["Bare", "gull", "og", "grønne", "skoger", "der", "jeg", "kommer", "fra", "for", "å", "si","det", "sånn"]
+		}
+	}
    render() {
+ 	// const bilde1 = "http://www.agria.no/imagevault/publishedmedia/9yazji5f3qwy3innewo0/tva-katter-myser.jpg";
+	// const bilde2 = "https://www.zoopermarked.no/resources/category/1_h/und/1%20hund.jpg?width=400&height=230&format=jpg";
       return (
-         <div>
-            
-         </div>
+      	<div>
+         <Dyr bilde={this.state.bilde1}/>
+      	 <Dyr bilde={this.state.bilde2}/>
+         <tabel>
+         	<th>DokumentID</th>
+     		<th>Dokumentnavn</th>
+     		<th>Datoer</th>
+     		<th>Kommuner</th>
+     		<th>Stedsnavn</th>
+     		<th>Mineraler</th>
+         	<TabellKolonner dok_id={this.state.dok_id} dok_navn={this.state.dok_navn} datoer={this.state.datoer} 
+         		kommuner={this.state.kommuner} stedsnavn={this.state.stedsnavn} mineraler={this.state.mineraler} />
+         	<TabellKolonner dok_id="2222" dok_navn="NAVN2" datoer={["20.02.2002"]} 
+    			kommuner={["Trondheim"]} stedsnavn={["Lade"]} mineraler={["Bare gull og grønne skoger"]} />
+
+         </tabel>
+        </div>
       );
    }
 }
---
+++ Components/TabellKolonner.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+class TabellKolonner <dok_id, dok_navn, datoer, kommuner, stedsnavn, mineraler > extends React.Component {
+   // linjeformatering() under er en alternativ fremvisningsmåte av informasjon.
+   linjeformatering(innData) {
+      var utData = "";
+      for(var i=0;i<innData.length; ++i) {
+         console.log("Dato nr: " + i);
+         if(i==0){
+            utData = innData[i];
+         } else {
+            utData = utData + ", " + innData[i] + "\n";
+         }
+      }
+      return (
+         <div>{utData}</div>
+      );
+   }
+
+   listeformatering(innData) {
+      const utData = innData.map((dat) => 
+         <dt key={dat}>{dat}</dt>
+      )
+      return (
+         <dl>{utData}</dl>
+      )
+   }
+
+   render() {
+      return (
+   		<tr>
+   			<td>{this.props.dok_id}</td>
+   			<td>{this.props.dok_navn}</td>
+            <td>{this.listeformatering(this.props.datoer)}</td>
+            <td>{this.listeformatering(this.props.kommuner)}</td>
+   			<td>{this.listeformatering(this.props.stedsnavn)}</td>
+   			<td>{this.linjeformatering(this.props.mineraler)}</td>
+   		</tr>
+      );
+   }
+}
+
+export default TabellKolonner;
