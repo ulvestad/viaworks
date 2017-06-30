d001a3b7ddcd18a457ce63e4ccaa11cd0773485a
Simen Ulvestad <simulves@dmf.int>
2017-06-30 08:39:41 +0200

    new and modifed ocr content scanner

--- httpNewThreading.py
+++ httpNewThreading.py
@@ -8,19 +8,20 @@ import time
 import multiprocessing
 from multiprocessing import Pool 
 from multiprocessing.dummy import Pool as ThreadPool 
+import time
 
 
 def threading(docnum):
 
 	start_time = time.time()
 
-	cookie = ".ASPXAUTH=78525415B015D500FF202DC008D961C3966EA7AF8005F1B10BC82644D3E7A9BD854F8D9956372DCD4F7F0264FE7BF48F0368C07D40A254E5AB67118363645F9F18FD529668BE02EC255C0045ECE491D30E6AE1FA2592F693D1EC77EE7DA79134728DC7A61CCA93B9FE8F1860D732D1F9CFC060B83543641276AD72138C5ECCCAAE4921B4BFB84BB477FC605B686067D49E270BA28C2635E683DA4B6D61270FB584FD3AE52DCB099D84F55208C4FB445FE0EB39221237680991D42596CBDA83FB36421ACAEFD9597DDF48EE70C68D2905F610F24C1BF21BF1B23AAB7E4316133BF7ADBF53E8595AAFECEA9B13BB3BF3FFBE75FE661110747922D7BF81529A2FD7073E93F3FE4612B1BFA885180789E40CEFB3B969F83950948977EF57FF78980A3D9C253EFCC1E8FEB9D2D45731073BC8E0F1B376A0CFCF09671958165FE84E017E7411297198705992AB810602E739CD203809B7F1AA06ED13C2CC97EE29D3ED973A75F5120C88CDCF84232A0A91200E24F5B0A4D1201FE1A64085F782D052755D8BA62ACED85F2ADC6E9C432E43A8240E5A2ECDEFE3302460CF3CCD67D3B06C0355E858; "
+	cookie = ".ASPXAUTH=7570AF3285570B1066D91825783243A1B8523212C15760FA8AB5015AFAFA26FBDCE4F8B87B54AEF24783938A8334B4AAB3A256A02276827EE4A9BD6E81171B7CB904EFC9C5D73A2A09A8C1BC30E94EEB9B9441F026182F76FB0A7988FE39DDC4CA72CB79A42016F0FF8A13251975522D370C70BAB2309EE3C025CB42C6AB15EDC46426314090BA435F446C60F1350C04C983B5FEB58AC851801BB0DB9D08A532E24A02B636BA8E9D9635149FC63D24C90B1B8BDFCE04F3FB204B13BA29898A612473840839EF4D265017AD81C6C17F1062618D5D30583F2BFB301CD7EC95E7FAFEAB05701C09F58F68FC1F43D2AF594F39B4D828EA9276F29969CE351922B8C85B693511C70ED91FB003C9B9E32E150AB6C259AB77B85CE8181C57F15A9DC8190E8A1F27588257F0B87F87BBD5E3D22213E7800E981624B9C6462193EE409FCAD0CC130C43B7783C544065AA2175C7F8C8A8258BC2961A4F47D2E92543EC049C8B88DA8B0CD30E666E99CB1CB31E76F91289B8CEA99B6ABA4C7C948E4578E2DD90DC3A59B89CE6EB3E414496FC01639DFFE3AD8CB2B5165D824C0BB2CED23CFB3176BE69; "
 	searchValue = "*"
 	docNum =  docnum
 
 
-
 	url1 = 'http://viaworks.dmf.int/RestService/4/api/Search?q='+searchValue+'%20vw(vwr(Source%3DKjellerarkiv%201%5C%5Cbv-rapporter%20samlet%205.5.2017))&r=6900&s=0&format=json&sort=score%20desc&lang=en-US&spid=0&df=&dt=&tags='
+	
 	r = requests.get(url1, headers={'Cookie': cookie}) # send auth unconditionally
 
 	r.raise_for_status() # raise an exception if the authentication fails
@@ -35,17 +36,7 @@ def threading(docnum):
 
 	documentId = content["Data"]["Hits"][docNum]["DocumentId"]
 	documentName = content["Data"]["Hits"][docNum]["Name"]
-	# cur.execute("SELECT * FROM data WHERE dok_id = %s", ([documentId]))
-	# preExist = cur.fetchall();
 
-	# file = open("log.txt", 'a')
-
-	# if preExist:
-	# 	file.write( str(docNum) +' ['+str(documentId) +', '+str(documentName)+'] is already scanned. Ignored.\n')
-	# 	file.close()
-	# if not preExist:
-	# 	file.write( str(docNum) +' ['+str(documentId) +', '+str(documentName)+'] scanned sucesfully. Result stored in \'data\' table.\n')
-	# 	file.close()
 
 	url2= 'http://viaworks.dmf.int/RestService/4/api/Document/convertedtext/'+str(documentId)+'/'
 	r2 = requests.get(url2, headers={'Cookie': cookie}) # send auth unconditionally
@@ -56,7 +47,13 @@ def threading(docnum):
 
 	documentContent = r2.content
 
+	file = open("log.txt", 'a')
 
+	if r2.text == "" or r2.text == None:
+		file.write( str(docNum) +' ['+str(documentId) +', '+str(documentName)+'] has no OCR content. (VIAWORKS has not indexed the file yet).\n')
+		file.close()
+		return None
+		
 	li = r2.text.split("\n")
 	ddmm = re.compile("^(0[1-9]|1[0-9]|2[0-9]|3[01])(0[1-9]|1[012])$")
 	yyyy = re.compile("^([0-9]{2}|(1[89][0-9]{2}|2[01][0-9]{2}))$")
@@ -83,9 +80,11 @@ def threading(docnum):
 	kom_li =[]
 	kom_sted = {}
 	
-	#dictonary for kommune(stedsnavn)
-	komm_i_rapport = komm_i_rapport[0][0].split("\n")
-	print(komm_i_rapport)	
+	#dictonary for kommune(stedsnavn
+	if komm_i_rapport != []:
+		if komm_i_rapport[0][0] != None:
+			komm_i_rapport = komm_i_rapport[0][0].split("\n")
+
 	for k in komm_i_rapport:
 		kom_li.append(k)
 		kom_sted[k] = None
@@ -93,23 +92,26 @@ def threading(docnum):
 	#check if already scanned
 	cur.execute("SELECT stedsnavn FROM data WHERE dok_id = %s",(documentId,))
 	isScanned = cur.fetchall()
-	if isScanned:
-		print("isScanned!! ")
+
+
 	
+
+
 	#if scanned only check stedsnavn againt kommune (avoids going thorugh line by line)
 	if isScanned:
+
 		cur.execute("SELECT mineraler FROM data WHERE dok_id = %s",(documentId,))
 		known_minerals = cur.fetchall()
-
+		
 		for ko in kom_li:
-			cur.execute("SELECT sted.enh_snavn FROM sted, kommune WHERE sted.enh_snavn = '"+st+"' AND sted.enh_komm = kommune.enh_komm AND kommune.enh_snavn =  %s",(ko,))
-			data = cur.fetchall()
-			if data:
-				for d in data:
-					if d[0] not in stedsnavn and d[0] != ko:
-						print(ko)
-						stedsnavn.append(d[0])
-						kom_sted[ko]=d[0]
+			for st in isScanned[0][0]:
+				cur.execute("SELECT sted.enh_snavn FROM sted, kommune WHERE sted.enh_snavn = '"+st+"' AND sted.enh_komm = kommune.enh_komm AND kommune.enh_snavn =  %s",(ko,))
+				data = cur.fetchall()
+				if data:
+					for d in data:
+						if d[0] not in stedsnavn and d[0] != ko:
+							stedsnavn.append(d[0])
+							kom_sted[ko]=d[0]
 	
 	#goes through line by line in document since it has not been scanned
 	else:
@@ -139,93 +141,27 @@ def threading(docnum):
 													stedsnavn.append(d[0])
 
 
-	cur.execute("INSERT INTO new_data (dok_id,dok_navn) VALUES ('%s','" + str(documentName) + "')",([documentId]))
-	
-	for mineral in known_minerals:
-		cur.execute("UPDATE new_data SET mineraler = mineraler || '{" + mineral + "}' WHERE dok_id = %s",([documentId]))
+	cur.execute("SELECT dok_id FROM new_data WHERE dok_id = %s",([documentId]))
+	exists = cur.fetchall()
+	if not exists:
 
-	for key in kom_sted:
-		insert_String = str(key)+"("+kom_sted.get(key)+")"
-		print(insert_String)
-		cur.execute("UPDATE new_data SET sted_i_kommmune = sted_i_kommmune || '{" + insert_String + "}' WHERE dok_id = %s",([documentId]))
+		cur.execute("INSERT INTO new_data (dok_id,dok_navn) VALUES ('%s','" + str(documentName) + "')",([documentId]))
+		
 
+		if known_minerals[0][0] != None:
+			for mineral in known_minerals[0][0]:
+				cur.execute("UPDATE new_data SET mineraler = mineraler || '{" + mineral + "}' WHERE dok_id = %s",([documentId]))
 
-	# print("\n")
-	# print(dok_navn_utenPDF)
-	# print(documentId)
-	# print(kom_li)
-	# print(kom_sted)
-	# print(stedsnavn)
-	# print(known_minerals)
+		for key in kom_sted:
+			if key != None:
+				if kom_sted.get(key) != None:
+					insert_String = str(key)+"("+kom_sted.get(key)+")"
+					cur.execute("UPDATE new_data SET sted_i_kommmune = sted_i_kommmune || '{" + insert_String + "}' WHERE dok_id = %s",([documentId]))
+				else:
+					if key[0] != None:
+						insert_String = str(key)+"( )"
+						cur.execute("UPDATE new_data SET sted_i_kommmune = sted_i_kommmune || '{" + insert_String + "}' WHERE dok_id = %s",([documentId]))
 
-
-
-
-	# 	if kommune_regex.match(line):
-	# 		ny_line = line.split(" ")
-	# 		for i in range(0,len(ny_line)):
-	# 			if ny_line[i].isalpha():
-	# 				cur.execute("SELECT * FROM kommune WHERE enh_snavn = '" + ny_line[i] + "'")
-	# 				ingenkommertilaalesedette = cur.fetchall()
-	# 				if ingenkommertilaalesedette:
-	# 					if ny_line[i] not in kommune_uten_sted:
-	# 						kommune_uten_sted.append(ny_line[i]);
-
-
-	# 	if ddmmyyyy.match(line):
-	# 		new_line = line.split(" ")
-
-	# 		for i in range(0, len(new_line)):
-	# 			if ddmmyyyy_excact.match(new_line[i]) and new_line[i] not in potential_dates:
-	# 				#print("\n"+new_line[i])
-	# 				potential_dates.append(new_line[i].replace("\r", ""))
-
-	# 		for i in range(0, len(new_line)-1,1):
-	# 			s = new_line[i]+" "+new_line[i+1]
-	# 			if ddmmyyyy_excact.match(s) and s not in potential_dates:
-	# 				#print("\n"+s)
-	# 				potential_dates.append(s.replace("\r", ""))
-
-	# 		for i in range(0, len(new_line)-2,1):
-	# 			s = new_line[i]+" "+new_line[i+1]+" "+new_line[i+2]
-	# 			if ddmmyyyy_excact.match(s) and s not in potential_dates:
-	# 				#print("\n"+s)
-	# 				potential_dates.append(s.replace("\r", ""))
-
-
-
-	# 	for mineral in mineraler:
-	# 		if(mineral.lower() in line.lower() and mineral not in known_minerals):
-	# 			known_minerals.append(mineral)
-
-	# 	strings = line.split(" ")
-	# 	for st in strings:
-	# 			if st != "":
-	# 				if sted_regex.match(st):		
-	# 						cur.execute("SELECT sted.enh_snavn, kommune.enh_snavn FROM sted, kommune WHERE sted.enh_snavn = '"+st+"' AND sted.enh_komm = kommune.enh_komm")
-	# 						data = cur.fetchall()
-	# 						if data:
-	# 							for d in data:
-	# 								if d[1] not in kommuner:
-	# 									kommuner.append(d[1])
-	# 								if d not in stedsnavn:
-	# 									stedsnavn.append(d)
-
-
-	# stedsdata = []
-	# dok_id = [documentId]
-	# cur.execute("INSERT INTO data (dok_id,dok_navn) VALUES ('%s','" + documentName + "')",(dok_id))
-	# for date in potential_dates:
-	# 	cur.execute("UPDATE data SET datoer = datoer || '{" + date + "}' WHERE dok_id = %s",(dok_id))
-	# for mineral in known_minerals:
-	# 	cur.execute("UPDATE data SET mineraler = mineraler || '{" + mineral + "}' WHERE dok_id = %s",(dok_id))
-	# for navn in stedsnavn:
-	# 	if navn[0] not in stedsdata:
-	# 		cur.execute("UPDATE data SET stedsnavn = stedsnavn || '{" + navn[0] + "}' WHERE dok_id = %s",(dok_id))
-	# 		stedsdata.append(navn[0])
-
-	# for kommune in kommune_uten_sted:
-	# 	cur.execute("UPDATE data SET kommunenr = kommunenr || '{" + kommune + "}' WHERE dok_id = %s",(dok_id))
 	
 	conn.commit();
 
@@ -233,13 +169,16 @@ def threading(docnum):
 	elapsed_time = time.time() - start_time
 	print ("Time elapsed: "+str(elapsed_time/60) + " min")
 
-
+	if isScanned:
+		file.write( str(docNum) +' ['+str(documentId) +', '+str(documentName)+'] is already scanned. Ignored. '+ str(elapsed_time/60)+'\n')
+		file.close()
+	else:
+		file.write( str(docNum) +' ['+str(documentId) +', '+str(documentName)+'] scanned succesfully. '+ str(elapsed_time/60)+'\n')
+		file.close()
 	cur.close()
 
 
-#start_total_time = time.time()
-#elapsed_total_time = time.time() - start_total_time
-#print("Total time elapsed: "+str(elapsed_total_time/60) + " min")
+
 
 if __name__ == '__main__':
 
